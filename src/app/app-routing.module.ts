303a86d9858c1604427802917cffebc80ce7f55c
GabrielReynaldo <gabrielreynaldo123@gmail.com>
2022-08-21 22:43:45 -0300

    logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { EstabelecimentoListComponent } from './components/estabelecimento/estabelecimento-list/estabelecimento-list.component';
 
 import { HomeComponent } from './components/home/home.component';
@@ -9,9 +10,12 @@ import { NavComponent } from './components/nav/nav.component';
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {
-    path: '', component : NavComponent, children: [
-      {path: 'home', component: HomeComponent},
+    path: '', component: NavComponent, canActivate: [AuthGuard], children: [
+      { path: 'home', component: HomeComponent },
+
       {path: 'estabelecimentos', component: EstabelecimentoListComponent}
+
+      
     ]
   }
   
