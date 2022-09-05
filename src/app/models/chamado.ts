d216c5fba1c38478a74ac9687ee8a963aa75bf5a
GabrielReynaldo <gabrielreynaldo123@gmail.com>
2022-09-05 16:55:00 -0300

    interface para chamado criada

---
+++ src/app/models/chamado.ts
@@ -0,0 +1,13 @@
+export interface Chamado {
+    id?:                any;
+    dataAbertura?:   string;
+    dataFechamento?: string;
+    prioridade:      string;
+    status:          string;
+    titulo:          string;
+    observacoes:     string;
+    estabelecimento:            any;
+    motoboy:            any;
+    nomeEstabelecimento:     string;
+    nomeMotoboy:     string;
+ }
