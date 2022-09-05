9b06438200330de94ccd151f4cb9c3729af1744b
GabrielReynaldo <gabrielreynaldo123@gmail.com>
2022-09-05 17:28:56 -0300

    html e css para component chamado list

--- src/app/components/estabelecimento/estabelecimento-list/estabelecimento-list.component.ts
+++ src/app/components/estabelecimento/estabelecimento-list/estabelecimento-list.component.ts
@@ -12,15 +12,6 @@ import { EstabelecimentoService } from 'src/app/services/estabelecimento.service
 export class EstabelecimentoListComponent implements OnInit {
 
   ELEMENT_DATA: Estabelecimento[] = [
- // {
-  //  id: 1,
-   // nome: 'Valdir Cezar',
-   // cpfcnpj: '123.456.789.10',
-  //  email: 'Valdir@email.com',
-   // senha: '123',
-   // perfis: ['0'],
-   // dataCriacao: '15/08/2022'
-  //}
   ]
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
   dataSource = new MatTableDataSource<Estabelecimento>(this.ELEMENT_DATA);
