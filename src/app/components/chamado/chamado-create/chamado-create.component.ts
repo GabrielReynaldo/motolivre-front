0b919e917d3221b20d4d33fea4d38ba9ac44af03
GabrielReynaldo <gabrielreynaldo123@gmail.com>
2022-09-07 14:58:19 -0300

    servico para criar chamado implementado

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Toast, ToastrService } from 'ngx-toastr';
+import { Chamado } from 'src/app/models/chamado';
+import { Estabelecimento } from 'src/app/models/estabelecimento';
+import { Motoboy } from 'src/app/models/motoboy';
+import { ChamadoService } from 'src/app/services/chamado.service';
+import { EstabelecimentoService } from 'src/app/services/estabelecimento.service';
+import { MotoboyService } from 'src/app/services/motoboy.service';
 
 @Component({
   selector: 'app-chamado-create',
@@ -8,20 +16,65 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class ChamadoCreateComponent implements OnInit {
 
+  chamado: Chamado = {
+    prioridade:  '',
+    status:      '',
+    titulo:      '',
+    observacoes: '',
+    estabelecimento:     '',
+    motoboy:     '',
+    nomeEstabelecimento: '',
+    nomeMotoboy: '',
+  }
+
+  motoboys: Motoboy[]=[];
+  estabelecimentos: Estabelecimento[]=[];
+
   prioridade: FormControl = new FormControl(null, [Validators.required]);
   status:     FormControl = new FormControl(null, [Validators.required]);
   titulo:     FormControl = new FormControl(null, [Validators.required]);
-  descricao:FormControl = new FormControl(null, [Validators.required]);
+  observacoes:FormControl = new FormControl(null, [Validators.required]);
   estabelecimento:    FormControl = new FormControl(null, [Validators.required]);
   motoboy:    FormControl = new FormControl(null, [Validators.required]);
 
-  constructor() { }
+  constructor(
+    private chamadoService:                 ChamadoService,
+    private motoboyService :                MotoboyService,
+    private estabelecimentoService: EstabelecimentoService,
+    private toastService:                    ToastrService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.findAllMotoboy();
+    this.findAllEstabelecimentos();
+  }
+
+  create():void{
+    this.chamadoService.create(this.chamado).subscribe(resposta =>{
+      this.toastService.success('Chamado criado com sucesso','Novo chamado');
+      this.router.navigate(['chamados']);
+    },ex =>{
+      console.log(ex);
+      this.toastService.error(ex.error.error)
+    })
+  }
+
+
+  findAllMotoboy(): void {
+    this.motoboyService.findAll().subscribe(resposta => {
+      this.motoboys = resposta;
+    })
+  }
+
+  findAllEstabelecimentos(): void {
+    this.estabelecimentoService.findAll().subscribe(resposta => {
+      this.estabelecimentos = resposta;
+    })
   }
 
   validaCampos(): boolean{
     return this.prioridade.valid && this.status.valid && this.titulo.valid 
-       && this.descricao.valid && this.estabelecimento.valid && this.motoboy.valid
+       && this.observacoes.valid && this.estabelecimento.valid && this.motoboy.valid
   }
 }
--- src/app/services/chamado.service.ts
+++ src/app/services/chamado.service.ts
@@ -14,4 +14,11 @@ export class ChamadoService {
   findAll(): Observable<Chamado[]>{
     return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`)
   }
+  create(chamado: Chamado): Observable<Chamado> {
+    return this.http.post<Chamado>(`${API_CONFIG.baseUrl}/chamados`, chamado);
+  }
+
+  update(chamado: Chamado): Observable<Chamado> {
+    return this.http.put<Chamado>(`${API_CONFIG.baseUrl}/chamados/${chamado.id}`, chamado);
+  }
 }
